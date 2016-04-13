8239d64693104be83a595e72cf5695b240ed62ec
Abhinav Ajgaonkar <abhinav316@gmail.com>
2016-04-13 02:53:32 -0400

    Absolute paths for mv

--- scripts/deploy-after-install.sh
+++ scripts/deploy-after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo chown -R ubuntu:ubuntu /tmp/go-to-sample
 sudo supervisorctl stop go-to-sample
-mv go-to-sample.bin /opt/go-to-sample/
+cp /tmp/go-to-sample/go-to-sample.bin /opt/go-to-sample/go-to-sample.bin
 sudo supervisorctl start go-to-sample
